4d6017972b9b696c560e021401881524df18e2b2
Abos2024 <abos2024@gmail.com>
2024-10-30 05:32:57 +0500

    Update script.js

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,13 +1,13 @@
 const lenis = new Lenis({
-  duration: 0.0,
-  smoothWheel: false,
-  smoothTouch: false,
-  normalizeWheel: true,
+	duration: 0.0,
+	smoothWheel: false,
+	smoothTouch: false,
+	normalizeWheel: true,
 });
 
 function raf(time) {
-  lenis.raf(time);
-  requestAnimationFrame(raf);
+	lenis.raf(time);
+	requestAnimationFrame(raf);
 }
 
 requestAnimationFrame(raf);
@@ -18,84 +18,85 @@ new Ukiyo(".ukiyo");
 });*/
 
 class AtroposComponent extends HTMLElement {
-  constructor() {
-    super();
-  }
+	constructor() {
+		super();
+	}
 
-  connectedCallback() {
-    this.atropos = new Atropos({
-      el: this.querySelector(".atropos"),
-      onEnter() {
-        console.log("Atropos Component: Enter");
-      },
-      onLeave() {
-        console.log("Atropos Component: Leave");
-      },
-      onRotate(x, y) {
-        console.log("Atropos Component: Rotate", x, y);
-      },
-    });
+	connectedCallback() {
+		this.atropos = new Atropos({
+			el: this.querySelector(".atropos"),
+			onEnter() {
+				console.log("Atropos Component: Enter");
+			},
+			onLeave() {
+				console.log("Atropos Component: Leave");
+			},
+			onRotate(x, y) {
+				console.log("Atropos Component: Rotate", x, y);
+			},
+		});
 
-    console.log("Atropos Component: Connected!", this);
-  }
+		console.log("Atropos Component: Connected!", this);
+	}
 
-  disconnectedCallback() {
-    this.atropos.destroy();
+	disconnectedCallback() {
+		this.atropos.destroy();
 
-    console.log("Atropos Component: Disconnected!", this);
-  }
+		console.log("Atropos Component: Disconnected!", this);
+	}
 }
 
 customElements.define("atropos-component", AtroposComponent);
 
 var menuButt = document.getElementById("menuButt");
 menuButt.onclick = function () {
-  openMenu();
+	openMenu();
 };
 var menuText = document.getElementById("menuButton");
 var menu = document.getElementById("menu");
 var navbar = document.getElementById("navbar");
 function openMenu() {
-  if (window.getComputedStyle(menu, null).getPropertyValue("width") === "0px") {
-    menuText.innerHTML = "close";
-    menu.style.width = "260px";
-    navbar.style.width = "calc(100% - 2vw - 260px)";
-    navbar.style.marginLeft = "calc(260px + 1vw)";
-  } else {
-    closeMenu();
-  }
+	if (window.getComputedStyle(menu, null).getPropertyValue("width") === "0px") {
+		menuText.innerHTML = "close";
+		menu.style.width = "260px";
+		navbar.style.width = "calc(100% - 2vw - 260px)";
+		navbar.style.marginLeft = "calc(260px + 1vw)";
+	} else {
+		closeMenu();
+	}
 }
+
 window.onscroll = () => {
-  scrollNavbar();
+	scrollNavbar();
 };
 
 scrollNavbar = () => {
-  var navBar = document.getElementById("navbar");
-  const nav = document.querySelectorAll("#nav");
-  if (document.documentElement.scrollTop > 50) {
-    navBar.classList.add("fixed-header");
-    for (let i = 0; i < links.length; i++) {
-      const element = links[i];
-      element.classList.add("scrolled-active");
-    }
-    navbar.style.width = "100%";
-    navbar.style.marginLeft = "0px";
-  } else {
-    navBar.classList.remove("fixed-header");
-    for (let i = 0; i < links.length; i++) {
-      const element = links[i];
-      element.classList.remove("scrolled-active");
-    }
-    navbar.style.width = "calc(100% - 2vw)";
-    navbar.style.marginLeft = "1vw";
-  }
+	var navBar = document.getElementById("navbar");
+	const nav = document.querySelectorAll("#nav");
+	if (document.documentElement.scrollTop > 50) {
+		navBar.classList.add("fixed-header");
+		for (let i = 0; i < links.length; i++) {
+			const element = links[i];
+			element.classList.add("scrolled-active");
+		}
+		navbar.style.width = "100%";
+		navbar.style.marginLeft = "0px";
+	} else {
+		navBar.classList.remove("fixed-header");
+		for (let i = 0; i < links.length; i++) {
+			const element = links[i];
+			element.classList.remove("scrolled-active");
+		}
+		navbar.style.width = "calc(100% - 2vw)";
+		navbar.style.marginLeft = "1vw";
+	}
 };
 
 function closeMenu() {
-  menuText.innerHTML = "menu";
-  menu.style.width = "0";
-  navbar.style.width = "calc(100% - 2vw)";
-  navbar.style.marginLeft = "1vw";
+	menuText.innerHTML = "menu";
+	menu.style.width = "0";
+	navbar.style.width = "calc(100% - 2vw)";
+	navbar.style.marginLeft = "1vw";
 }
 var rellax = new Rellax(".relax");
 window.frames[1].stop();
